9fd56422a8942a379444cf421287430c1a45dbdb
NgollrRomaric <ngolleromaric27@gmail.com>
2025-03-20 06:01:25 +0100

    Initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+import { cva } from "class-variance-authority";
+import { ButtonHTMLAttributes } from "react";
+
+const classes = cva("border h-12 rounded-full px-6 text-medium", {
+    variants: {
+        variant: {
+            primary: "bg-lime-400 text-neutral-950 border lime-400",
+            secondary: "border-white text-white bg-tranparent",
+        },
+        size: {
+            sm: "h-10",
+        },
+    },
+});
+
+export default function Button(
+    props: {
+        variant: "primary" | "secondary";
+        size?: "sm";
+    } & ButtonHTMLAttributes<HTMLButtonElement>
+) {
+    const { variant, className, size, ...otherProps } = props;
+    return (
+        <button
+            className={classes({
+                variant,
+                size,
+                className,
+            })}
+            {...otherProps}
+        />
+    );
+}
---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
+// import { link } from "fs"; // Commented out as it's not being used
+
+const navLinks = [
+    { label: "Home", href: "#" },
+    { label: "Features", href: "#features" },
+    { label: "Integrations", href: "#integrations" },
+    { label: "FAQs", href: "#faqs" },
+];
+
+export default function Navbar() {
+    return (
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+                    <div>
+                        <Image
+                            src={logoImage}
+                            alt="Layers logo"
+                            className="w-auto h-9 md:h-auto"
+                        />
+                    </div>
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map((link) => (
+                                <a href={link.href} key={link.label}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="prefix__feather prefix__feather-menu md:hidden"
+                        >
+                            <path d="M3 12h18M3 6h18M3 18h18" />
+                        </svg>
+                        <Button
+                            variant="secondary"
+                            className="hidden md:inline-flex items-center"
+                        >
+                            Log In
+                        </Button>
+                        <Button
+                            variant="primary"
+                            className="hidden md:inline-flex items-center"
+                        >
+                            Sign In
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
